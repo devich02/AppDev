e0133ccb2c781f53c3401c55638b066d7698c9b1
Giancarlo Devich <devich02@gmail.com>
2015-05-18 16:18:26 -0700

    Game of life test app in C#

---
+++ TestApps/GameOfLife/GameOfLife/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(283, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 38);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Iteration delay (ms)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestApps/GameOfLife/GameOfLife/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+
+        int GridSquareSize = 20;
+
+        class Cell
+        {
+
+            public enum CellState
+            {
+                Alive,
+                Dead
+            }
+
+            public CellState m_State = CellState.Dead;
+            public CellState m_NextState = CellState.Dead;
+
+            public Cell()
+            {
+
+            }
+
+
+        }
+
+        Cell[][] CellGrid;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ClientSize = new Size(800, 700);
+            CellGrid = new Cell[800 / GridSquareSize][];
+            for (int i = 0; i < CellGrid.Length; ++i)
+            {
+                CellGrid[i] = new Cell[600 / GridSquareSize];
+            }
+
+            for (int i = 0; i < CellGrid.Length; ++i)
+            {
+                for (int j = 0; j < CellGrid[i].Length; ++j)
+                {
+                    CellGrid[i][j] = new Cell();
+                }
+            }
+        }
+
+        Cell GetCell(int x, int y)
+        {
+            if (x < 0)
+                x = CellGrid.Length + x;
+            if (x >= CellGrid.Length)
+                x -= CellGrid.Length;
+            if (y < 0)
+                y = CellGrid[x].Length + y;
+            if (y >= CellGrid[x].Length)
+                y -= CellGrid[x].Length;
+
+            return CellGrid[x][y];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        int mx = 0, my = 0;
+
+        bool mdown = false;
+        MouseButtons mbutton = MouseButtons.Left;
+        bool brunning = false;
+        bool bstepping = false;
+        private long llIterationDelay = 0;
+        long llLastUpdate = 0;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(0, 100);
+
+            int rx = mx / GridSquareSize;
+            int ry = my / GridSquareSize;
+
+            e.Graphics.FillRectangle(Brushes.Orange, rx * GridSquareSize, ry * GridSquareSize, GridSquareSize, GridSquareSize);
+
+            if (mdown)
+            {
+                if (rx >= 0 && rx < CellGrid.Length &&
+                    ry >= 0 && ry < CellGrid[rx].Length)
+                {
+                    if (mbutton == MouseButtons.Left)
+                        CellGrid[rx][ry].m_State = Cell.CellState.Alive;
+                    else
+                        CellGrid[rx][ry].m_State = Cell.CellState.Dead;
+                }
+            }
+
+            for (int i = 0; i < CellGrid.Length; ++i)
+            {
+                for (int j = 0; j < CellGrid[i].Length; ++j)
+                {
+                    if (CellGrid[i][j].m_State == Cell.CellState.Alive)
+                    {
+                        e.Graphics.FillRectangle(Brushes.LightGreen, i * GridSquareSize, j * GridSquareSize, GridSquareSize, GridSquareSize);
+                    }
+                }
+            }
+
+            for (int i = 0; i < this.Width; i += GridSquareSize)
+            {
+                e.Graphics.DrawLine(Pens.Black, i, 0, i, this.Height);
+            }
+
+            for (int i = 0; i < this.Height; i += GridSquareSize)
+            {
+                e.Graphics.DrawLine(Pens.Black, 0, i, this.Width, i);
+            }
+
+            if (brunning  && (DateTime.Now.Ticks - llLastUpdate) > llIterationDelay)
+            {
+
+                llLastUpdate = DateTime.Now.Ticks;
+
+                for (int i = 0; i < CellGrid.Length; ++i)
+                {
+                    for (int j = 0; j < CellGrid[i].Length; ++j)
+                    {
+                        Cell top_left = GetCell(i - 1, j - 1);
+                        Cell top = GetCell(i, j - 1);
+                        Cell top_right = GetCell(i + 1, j - 1);
+
+                        Cell right = GetCell(i + 1, j);
+
+                        Cell bottom_right = GetCell(i + 1, j + 1);
+                        Cell bottom = GetCell(i, j + 1);
+                        Cell bottom_left = GetCell(i - 1, j + 1);
+
+                        Cell left = GetCell(i - 1, j);
+
+                        int count = 0;
+
+                        count += (top_left != null && top_left.m_State == Cell.CellState.Alive) ? 1 : 0;
+                        count += (top != null && top.m_State == Cell.CellState.Alive) ? 1 : 0;
+                        count += (top_right != null && top_right.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                        count += (right != null && right.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                        count += (bottom_right != null && bottom_right.m_State == Cell.CellState.Alive) ? 1 : 0;
+                        count += (bottom != null && bottom.m_State == Cell.CellState.Alive) ? 1 : 0;
+                        count += (bottom_left != null && bottom_left.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                        count += (left != null && left.m_State == Cell.CellState.Alive) ? 1 : 0;
+                        
+                        if ((count == 2 || count == 3) && CellGrid[i][j].m_State == Cell.CellState.Alive)
+                        {
+                            CellGrid[i][j].m_NextState = Cell.CellState.Alive;
+                        }
+                        else if (count < 2)
+                        {
+                            CellGrid[i][j].m_NextState = Cell.CellState.Dead;
+                        }
+                        else if (count > 3)
+                        {
+                            CellGrid[i][j].m_NextState = Cell.CellState.Dead;
+                        }
+                        else if (count == 3 && CellGrid[i][j].m_State == Cell.CellState.Dead)
+                        {
+                            CellGrid[i][j].m_NextState = Cell.CellState.Alive;
+                        }
+                     
+                    }
+                }
+
+
+                for (int i =0; i < CellGrid.Length; ++i)
+                {
+                    for (int j = 0; j < CellGrid[i].Length; ++j)
+                    {
+                        CellGrid[i][j].m_State = CellGrid[i][j].m_NextState;
+                    }
+                }
+            }
+
+            if (bstepping && brunning)
+            {
+                brunning = false;
+            }
+
+            /*
+            for (int i = 0; i < CellGrid.Length; ++i)
+            {
+                for (int j = 0; j < CellGrid[i].Length; ++j)
+                {
+                    Cell top_left = GetCell(i - 1, j - 1);
+                    Cell top = GetCell(i, j - 1);
+                    Cell top_right = GetCell(i + 1, j - 1);
+
+                    Cell right = GetCell(i + 1, j);
+
+                    Cell bottom_right = GetCell(i + 1, j + 1);
+                    Cell bottom = GetCell(i, j + 1);
+                    Cell bottom_left = GetCell(i - 1, j + 1);
+
+                    Cell left = GetCell(i - 1, j);
+
+                    int count = 0;
+
+                    count += (top_left != null && top_left.m_State == Cell.CellState.Alive) ? 1 : 0;
+                    count += (top != null && top.m_State == Cell.CellState.Alive) ? 1 : 0;
+                    count += (top_right != null && top_right.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                    count += (right != null && right.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                    count += (bottom_right != null && bottom_right.m_State == Cell.CellState.Alive) ? 1 : 0;
+                    count += (bottom != null && bottom.m_State == Cell.CellState.Alive) ? 1 : 0;
+                    count += (bottom_left != null && bottom_left.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                    count += (left != null && left.m_State == Cell.CellState.Alive) ? 1 : 0;
+
+                    e.Graphics.DrawString("" + count, new Font("Courier New", 8), Brushes.Black, i * GridSquareSize, j * GridSquareSize);
+
+
+                }
+            }*/
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mdown = true;
+            mbutton = e.Button;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mdown = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (brunning)
+                {
+                    brunning = false;
+                }
+                else
+                {
+                    brunning = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                if (bstepping)
+                {
+                    bstepping = false;
+                }
+                else
+                {
+                    bstepping = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            llIterationDelay = (long)(numericUpDown1.Value * 10000);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mx = e.X;
+            my = e.Y - 100;
+        }
+    }
+}
